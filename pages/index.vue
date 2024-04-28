c50d099313b2679b550dba7578dd89c36d596f29
Kenny Cartman <lyuholu@gmail.com>
2024-04-28 22:00:18 +0800

    feat: allow users to set their usernames

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
+import { getData } from 'nuxt-storage/local-storage';
 import type { RouteLocationNormalizedLoaded } from 'vue-router';
 
 const route: RouteLocationNormalizedLoaded = useRoute();
 const isActive: boolean = route.fullPath === '/';
-const { data: users } = await useFetch(
-  'https://mocki.io/v1/fdb05063-4e7d-4db0-a7d6-a0e30eebd942'
-);
+// const { data: users } = await useFetch(
+//   'https://mocki.io/v1/fdb05063-4e7d-4db0-a7d6-a0e30eebd942'
+// );
 </script>
 
 <template>
   <header>
     <nav>
       <ul>
-        <li :class="{ active: isActive }"><NuxtLink to="/">Home</NuxtLink></li>
+        <li :class="{ active: isActive }">
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
         <li><NuxtLink to="/settings">Settings</NuxtLink></li>
       </ul>
     </nav>
   </header>
   <main>
-    <section>{{ users }}</section>
+    <section>
+      <h1>
+        👋 {{ getData('username') ? getData('username') : 'Anonymous User' }}
+      </h1>
+    </section>
   </main>
   <footer></footer>
 </template>
--- pages/settings.vue
+++ pages/settings.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import { setData } from 'nuxt-storage/local-storage';
 import type { RouteLocationNormalizedLoaded } from 'vue-router';
 
+const username: globalThis.Ref<string> = ref('');
 const route: RouteLocationNormalizedLoaded = useRoute();
 const isActive: boolean = route.fullPath === '/settings';
+
+function handleUsername(event) {
+  username.value = event.target.value;
+}
+
+function save() {
+  setData('username', username.value);
+}
+
+function forget() {
+  setData('username', '');
+}
 </script>
 
 <template>
@@ -16,6 +30,22 @@ const isActive: boolean = route.fullPath === '/settings';
       </ul>
     </nav>
   </header>
-  <main>Settings Page</main>
+  <main>
+    <h1>Settings</h1>
+    <section>
+      <section class="setting">
+        <label for="username">Username</label>
+        <input
+          name="username"
+          type="text"
+          :value="username"
+          placeholder="Username"
+          @input="handleUsername"
+        />
+      </section>
+      <button class="save-btn" @click="save">Save</button>
+      <button class="forget-btn" @click="forget">Forget</button>
+    </section>
+  </main>
   <footer></footer>
 </template>
