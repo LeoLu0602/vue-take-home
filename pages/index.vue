aafbfa075f9de01f24b8d05950a99ad2e6006f4d
Kenny Cartman <lyuholu@gmail.com>
2024-04-28 20:46:05 +0800

    feat: finish navigation bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-const users = await useFetch(
+import type { RouteLocationNormalizedLoaded } from 'vue-router';
+
+const route: RouteLocationNormalizedLoaded = useRoute();
+const isActive: boolean = route.fullPath === '/';
+const { data: users } = await useFetch(
   'https://mocki.io/v1/fdb05063-4e7d-4db0-a7d6-a0e30eebd942'
 );
 </script>
 
 <template>
-  <div>
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/settings">Settings</NuxtLink></li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-      Home Page
-      <section>{{ users }}</section>
-    </main>
-    <footer></footer>
-  </div>
+  <header>
+    <nav>
+      <ul>
+        <li :class="{ active: isActive }"><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/settings">Settings</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section>{{ users }}</section>
+  </main>
+  <footer></footer>
 </template>
--- pages/settings.vue
+++ pages/settings.vue
@@ -1,9 +1,18 @@
+<script setup lang="ts">
+import type { RouteLocationNormalizedLoaded } from 'vue-router';
+
+const route: RouteLocationNormalizedLoaded = useRoute();
+const isActive: boolean = route.fullPath === '/settings';
+</script>
+
 <template>
   <header>
     <nav>
       <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/settings">Settings</NuxtLink></li>
+        <li :class="{ active: isActive }">
+          <NuxtLink to="/">Settings</NuxtLink>
+        </li>
       </ul>
     </nav>
   </header>
